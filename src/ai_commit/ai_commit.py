786a2fc9d1904634a92094da50249fdc9f7420d2
Frank Levine <frank.levine@jacobs.com>
2023-11-23 09:35:29 -0500

    checkpoint

--- src/ai_commit/ai_commit.py
+++ src/ai_commit/ai_commit.py
@@ -1,7 +1,9 @@
 import subprocess
 import openai
+from openai import OpenAI
 import click
 import os
+import tempfile
 
 @click.command()
 def main():
@@ -21,7 +23,7 @@ def main():
         return
 
     commit_message = generate_commit_message(changes)
-    click.echo(commit_message)
+    edit_commit_message(commit_message)
 
 def get_git_changes():
     result = subprocess.run(['git', 'diff', '--cached'], capture_output=True, text=True)
@@ -29,15 +31,65 @@ def get_git_changes():
 
 def generate_commit_message(changes):
     try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt="Generate a git commit message for these changes:\n\n" + changes,
-            max_tokens=60
+        client = OpenAI()
+        print("Generating commit message...")
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {
+                    "role": "user",
+                    "content": """You are writing one git commit message based on git diffs.
+                    
+                    The format of the message should be:
+                    <<summary line (60 chars max)>>
+                    <<blank line>>
+                    <<paragraph describing the changes>>
+                    
+                    Notes:
+                    add newlines in the paragraph to keep the max line length at 80 characters
+                    describe changes imperitively
+                    do not mention changes to imports
+                    do not mention that the message was geneated by AI"""
+                },
+                {
+                    "role": "user",
+                    "content": "here is the diff: " + changes
+                }
+            ],
+            stream=True
         )
-        return response.choices[0].text.strip()
+        
+        commit_message = ""
+        for part in stream:
+            commit_message += part.choices[0].delta.content or ""
+
+        return commit_message
     except Exception as e:
         click.echo("Error in generating commit message: " + str(e), err=True)
         exit(1)
+
+def edit_commit_message(commit_message):
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp_file:
+        tmp_file.write(commit_message)
+        tmp_file_path = tmp_file.name
+
+    editor = subprocess.check_output(['git', 'var', 'GIT_EDITOR']).strip().decode('utf-8')
+    subprocess.call([editor, tmp_file_path])
+
+    with open(tmp_file_path, 'r') as file:
+        final_commit_message = ""
+        for line in file:
+            if not line.strip().startswith('#'):
+                final_commit_message += line
+
+
+    if not final_commit_message.strip():
+        click.echo("Commit aborted: no commit message provided after editing.")
+        return
+
+    subprocess.call(['git', 'commit', '-F', tmp_file_path])
+    os.unlink(tmp_file_path)
+
 
 if __name__ == "__main__":
     main()
